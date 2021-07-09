99723d26010dfb5decb3c3238d5e06b6ce006986
ryan <1669971502@qq.com>
2021-07-09 18:29:46 +0800

    更新

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.contrib import auth
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from rest_framework import status
 from rest_framework.authtoken import serializers
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from Blog.models import User
@@ -36,8 +40,15 @@ class Login(APIView):
         if serializer.is_valid():
             user = auth.authenticate(**serializer.data)
             if user:
-                auth.login(request, user)
-                return Response(data=serializer.response(msg='登录成功', data=serializer.data))
+                # auth.login(request, user)
+                token, created = Token.objects.update_or_create(user=user)
+                return Response(data=serializer.response(msg='登录成功', data={'Authorization': ' Token ' + token.key}))
             else:
-                return Response(data=serializer.response(msg='用户名或密码错误'))
+                return Response(data=serializer.response(msg='用户名或密码错误'), status=status.HTTP_403_FORBIDDEN)
         return Response(data=serializer.response())
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('user/', views.ManageUser.as_view()),
     path('register/', views.Register.as_view()),
     path('login/', views.Login.as_view()),
+    path('auth/', views.create_auth_token)
 ]
